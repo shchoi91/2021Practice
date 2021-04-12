ba2bc2dac002a3d32d789a426eacef2a9439cb64
shchoi <shchoicoder@gmail.com>
2021-04-12 20:21:30 +0900

    First day for reviewing Java

---
+++ eclipse-workspace/JSP/2021Practice/src/Day1Bigger.java
@@ -0,0 +1,54 @@
+import java.util.*;
+//오름차순 정렬하기
+public class Day1Bigger {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	int num1 , num2, num3, num4 =0;
+	int tmp=0;
+	System.out.println("첫번째 수를 입력하세요.");
+	num1 = sc.nextInt();
+	System.out.println("두번째 수를 입력하세요.");
+	num2 = sc.nextInt();
+	System.out.println("세번째 수를 입력하세요.");
+	num3 = sc.nextInt();
+	System.out.println("네번째 수를 입력하세요.");
+	num4 = sc.nextInt();
+		
+		if(num1>num2) {
+			tmp = num1;
+			num1 = num2;
+			num2 = tmp;
+		}
+		if(num1>num3) {
+			tmp = num1;
+			num1 = num3;
+			num3 = tmp;
+		}
+		if(num1>num4) {
+			tmp = num1;
+			num1 = num4;
+			num4 = tmp;
+		}
+		//num1이 제일 작음.
+		if(num2>num3) {
+			tmp = num2;
+			num2 = num3;
+			num3 = tmp;
+		}
+		if(num2>num4) {
+			tmp = num2;
+			num2 = num4;
+			num4 = tmp;
+		}
+		if(num3>num4) {
+			tmp = num3;
+			num3 = num4;
+			num4 = tmp;
+		}
+		System.out.print("결과는...");
+		System.out.println(num1+","+num2+","+num3+","+num4+"입니다.");
+	}
+
+}
