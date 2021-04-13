cd6970e5d09b904569fa2ba4591e6e64208c7aa7
shchoi <shchoicoder@gmail.com>
2021-04-13 17:44:33 +0900

    Trying to use Git

---
+++ src/Day1For.java
@@ -0,0 +1,47 @@
+
+public class Day1For {
+	/*
+	 public static void main(String[] args) {
+		
+		
+		int sum = 0;
+		int sum2 = 0;
+		int count = 0;
+		int multiValue = 1;
+		int sum3 = 0;
+		int count2 = 0;
+		
+		//1부터 100까지의 합
+		
+		for(int i=1;i<101;i++) {
+			sum+=i;
+			
+		}
+		for(int i=1;i<101;i++) {
+			if(i%2==0) {
+				sum2+=i;
+			}
+		}
+		for(int i=1;i<101;i++) {
+			if(i%4==0) {
+				count++;
+			}
+		}
+		for(int i=1;i<6;i++) {
+			multiValue *=i;
+		}
+		System.out.println("1부터 100까지의 합은: "+sum);
+		System.out.println("1부터 100까지 짝수의 합은: "+sum2);
+		System.out.println("1부터 100까지 4의 배수 갯수는: "+count+"개");
+		System.out.println("5!은: "+multiValue);
+	
+		int sum3=0, count3=0;
+
+		for(i=7;sum<=1000;i=i+7)
+		{
+			sum3 = sum3 + i;
+			count3=count3+1;
+			System.out.println("count=" + count3 + " i=" + i + " sum=" + sum3);
+		}
+	}	*/
+}
---
+++ src/Day2ContinueAndBreak.java
@@ -0,0 +1,56 @@
+
+public class Day2ContinueAndBreak {
+
+	/*
+	public static void main(String[] args) {
+
+		
+		 //최초로 5000을 넘는 수를 break를 통해 구현.
+		for(int i=1;true;i++) {
+			
+			if(i>5000) {
+				System.out.println(i);
+				break;
+			}
+		}
+		
+		//1부터 20까지 출력을 하는데 2의 배수와 3의 배수는 출력하지 않기 continue 이용
+		for(int i=1;i<21;i++) {
+			
+			if(i%2==0||i%3==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+		//5의 배수이자 7의 배수인 가장 작은 수 break
+		for(int i=1;true;i++) {
+			if(i%5==0&&i%7==0) {
+				System.out.println(i);
+				break;
+			}
+			
+		}
+		
+		
+		//100 이하의 자연수 중에 5의 배수이자 7의 배수인 정수를 전부출력하고, 그 수를 세어보는 프로그램 continue;
+		
+		int count=0;
+		for(int i=1;i<=100;i++) {
+			
+			if(i%5==0&&i%7==0) {
+				System.out.println(i);
+				count++;
+				
+			}
+			else {
+				continue;
+			}
+			
+		}
+		System.out.println(count);
+	}
+	
+	*/
+}
